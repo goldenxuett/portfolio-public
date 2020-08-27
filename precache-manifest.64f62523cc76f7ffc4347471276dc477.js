08757b5b866d1a16ece21cf4f9f55372a8c73f7a
goldenxuett <ttanpcs@gmail.com>
2020-08-26 21:18:59 -0500

    Updates

---
+++ precache-manifest.64f62523cc76f7ffc4347471276dc477.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "835215036019b70bd8ecdd894f64af61",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6de66e5809e68381d4e5",
+    "url": "/static/css/main.d91e01c7.chunk.css"
+  },
+  {
+    "revision": "81c2959f5e894d9f55a8",
+    "url": "/static/js/2.1a25b51c.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.1a25b51c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6de66e5809e68381d4e5",
+    "url": "/static/js/main.7a03fcd2.chunk.js"
+  },
+  {
+    "revision": "697324bdaddf95d2b7f3",
+    "url": "/static/js/runtime-main.a4cfdc90.js"
+  },
+  {
+    "revision": "92e0361d6c76a97f35d23e7517de1f42",
+    "url": "/static/media/2002ux.92e0361d.pdf"
+  },
+  {
+    "revision": "d2ae810462fdb572ca16c716bfdde321",
+    "url": "/static/media/2002ux_image.d2ae8104.png"
+  },
+  {
+    "revision": "f282dadec0f37aa0aa0dd27e37819b6f",
+    "url": "/static/media/cs.f282dade.jpg"
+  },
+  {
+    "revision": "df23cd64e8b7a32a4d9ddd69a69a4ee3",
+    "url": "/static/media/cyclic.df23cd64.pdf"
+  },
+  {
+    "revision": "a0bf0cfd283412e735ce15b6ebdfc179",
+    "url": "/static/media/cyclic_image.a0bf0cfd.png"
+  },
+  {
+    "revision": "73adc9ffaf75383691c0ebbaf4019958",
+    "url": "/static/media/first.73adc9ff.png"
+  },
+  {
+    "revision": "de196eb29d4e5a0a5d8f7819e7f6706f",
+    "url": "/static/media/food.de196eb2.pdf"
+  },
+  {
+    "revision": "f598d9ab3ff72f7dd89c1b8d4b84c49a",
+    "url": "/static/media/food_image.f598d9ab.png"
+  },
+  {
+    "revision": "51cfabeef9ee69f5102bb477acbc3183",
+    "url": "/static/media/fumes.51cfabee.pdf"
+  },
+  {
+    "revision": "4a3932bb9d0e53b410304a3fc0b8870c",
+    "url": "/static/media/fumes_image.4a3932bb.png"
+  },
+  {
+    "revision": "bbd8e551c845caa1628c08b559514303",
+    "url": "/static/media/pi_image.bbd8e551.png"
+  },
+  {
+    "revision": "76e5f339f974bce05640d3b7f755513a",
+    "url": "/static/media/second.76e5f339.jpg"
+  },
+  {
+    "revision": "812a0c578bd779694c1ca7809a50123d",
+    "url": "/static/media/secure_image.812a0c57.png"
+  },
+  {
+    "revision": "4883ce3b4c6f84be7f11767b7f428baa",
+    "url": "/static/media/third.4883ce3b.jpg"
+  },
+  {
+    "revision": "0dbe8b41fb5e274eb7170c71a6499cd4",
+    "url": "/static/media/weed.0dbe8b41.pdf"
+  },
+  {
+    "revision": "67b78adac73c47589fe62cd5aa198089",
+    "url": "/static/media/weed_image.67b78ada.png"
+  },
+  {
+    "revision": "36a527690b030cc3a699988cab3121ce",
+    "url": "/static/media/yspa.36a52769.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/ttanpcs.com/precache-manifest.e0ef63bbc3d7fd74c4b0c6799623437e.js"
+  "/precache-manifest.64f62523cc76f7ffc4347471276dc477.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/ttanpcs.com/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
